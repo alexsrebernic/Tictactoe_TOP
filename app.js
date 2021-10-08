698edd4a57d1420e8683158bc0b6648541247f8d
Alex Srebernic <alexsrebernic@gmail.com>
2021-10-08 16:19:39 -0300

    finish ai

--- app.js
+++ app.js
@@ -81,8 +81,12 @@ function restartGameBoard(){
     winsAi.textContent = ''
     winsPlayer1.textContent = ''
     winsPlayer2.textContent = ''
+    player1.setTurn(true)
+    player2.setTurn(false)
+    machine.setTurn(false)
     player1.setArray([])  
     player2.setArray([]) 
+    machine.setArray([])
     gamesIsOver = false
     gameBoard.resetBoard()
     setMessage("")
@@ -94,39 +98,76 @@ function restartGameBoard(){
 
 
 function putSignOnBox(e){
-    if(e.target.textContent !== '' || gamesIsOver) return
-    if(player1.getTurn()){
-        setSignInArray(player1,player2,"Its turn of Player 2",e)
-    } else if (player2.getTurn()){
-        setSignInArray(player2,player1,"Its turn of Player 1",e)
-    }
-    signs += 1
-    if(checkWinner.checkPlayerWinner(player1.getArray())){
-        resetSettings(winsPlayer1,"Player 1 Wins The Round!",scorePlayer1)
-        scorePlayer1 += 1
-    }
-    if(checkWinner.checkPlayerWinner(player2.getArray())){
-        resetSettings(winsPlayer2,"Player 2 Wins The Round!",scorePlayer2)   
-        scorePlayer2 += 1
+    if(player2Side.style.display == "flex"){
+        if(e.target.textContent !== '' || gamesIsOver) return
+        if(player1.getTurn()){
+            setSignInArray(player1,player2,"Its turn of Player 2",e)
+        } else if (player2.getTurn()){
+            setSignInArray(player2,player1,"Its turn of Player 1",e)
+        }
+        signs += 1
+        if(checkWinner.checkPlayerWinner(player1.getArray())){
+            resetSettings(winsPlayer1,"Player 1 Wins The Round!")
+            scorePlayer1 += 1
+        }
+        if(checkWinner.checkPlayerWinner(player2.getArray())){
+            resetSettings(winsPlayer2,"Player 2 Wins The Round!")   
+            scorePlayer2 += 1
 
-    }
-    if(signs == 9){
-        resetSettings(null,"It's a tie!",null)
-       
-    }
-    if(scorePlayer1 > 2 && scorePlayer1 === scorePlayer2 + 2 || scorePlayer1 === 3 && scorePlayer2 === 0){
-        playerWins("Player 1 Wins!")
-    }
-    if(scorePlayer2 > 2 && scorePlayer2 === scorePlayer1 + 2 || scorePlayer2 === 3 && scorePlayer1 === 0){
-        playerWins("Player 2 Wins!")
+        }
+        if(signs == 9) resetSettings(null,"It's a tie!",null)
 
+        if(scorePlayer1 > 2 && scorePlayer1 === scorePlayer2 + 2 || scorePlayer1 === 3 && scorePlayer2 === 0) playerWins("Player 1 Wins!")
+
+        if(scorePlayer2 > 2 && scorePlayer2 === scorePlayer1 + 2 || scorePlayer2 === 3 && scorePlayer1 === 0) playerWins("Player 2 Wins!")
+
+    } else if (aiSide.style.display === "flex"){
+        if(e.target.textContent !== '' || gamesIsOver)return
+        if(player1.getTurn()){
+            setMessage("")
+            player1.getArray().push(parseInt(e.target.dataset.index))
+            e.target.textContent = player1.getSign()
+            gameBoard.setBox(parseInt(e.target.dataset.index),player1.getSign())
+            signs += 1
+            
+            let arrayAvalaiblePositions = gameBoard.getBoard()
+            .map((n,i) => {
+                if(n == ""){
+                    return i
+                }
+            })
+            .filter(n => {
+                return n != undefined
+            })
+                // The AI starts with a random position
+                
+                let randomPosition = Math.floor(Math.random() * arrayAvalaiblePositions.length)
+                machine.getArray().push(arrayAvalaiblePositions[randomPosition])
+                if(boxesBoard[arrayAvalaiblePositions[randomPosition]] !== undefined) boxesBoard[arrayAvalaiblePositions[randomPosition]].textContent = machine.getSign()
+                gameBoard.setBox(arrayAvalaiblePositions[randomPosition],machine.getSign())
+            signs += 1
+                
+            if(checkWinner.checkPlayerWinner(player1.getArray())){
+                resetSettings(winsPlayer1,"Player 1 Wins The Round!")
+                scorePlayer1 += 1
+            }
+            if(checkWinner.checkPlayerWinner(machine.getArray())){
+                resetSettings(winsAi,"The AI Wins The Round!")
+                scoreAi += 1
+            }
+            if(signs >= 9) resetSettings(null,"It's a tie!",null)
+            if(scorePlayer1 > 2 && scorePlayer1 === scoreAi + 2 || scorePlayer1 === 3 && scoreAi === 0) playerWins("Player 1 Wins!")
+
+            if(scoreAi > 2 && scoreAi === scorePlayer1 + 2 || scoreAi === 3 && scorePlayer1 === 0) playerWins("The AI Wins!")  
+        }
     }
+    
 }
 function playerWins(message){
     restartGameBoard()
     setMessage(message)
-
 }
+
 function setMessage(str){
     let para = document.getElementById("player_x_turn_text")
     para.textContent = str
@@ -143,6 +184,7 @@ function setImg(player){
 function resetSettings(player,message){
     player2.setArray([])
     player1.setArray([])
+    machine.setArray([])
     setMessage(message) 
     gameBoard.resetBoard()
     boxesBoard.forEach(box => {
@@ -152,6 +194,7 @@ function resetSettings(player,message){
     signs= 0
     if(player != null) setImg(player)
 }
+
 function setSignInArray(winPlayer,loosePlayer,message,e){
     winPlayer.getArray().push(parseInt(e.target.dataset.index))
     e.target.textContent = winPlayer.getSign()
@@ -160,6 +203,7 @@ function setSignInArray(winPlayer,loosePlayer,message,e){
     loosePlayer.setTurn(true)
     setMessage(message)
 }
+
 function Player(sign,turn,wins,array){
     this.sign = sign
     this.turn = turn
@@ -189,7 +233,6 @@ function Player(sign,turn,wins,array){
     const setArray = (newArray) => {
         return array = newArray
     }
-
     return { getSign,getTurn,getWins,getArray,setArray,setSign,setTurn,setWins }
 }
 const player1 = Player("X",true,0,[])
@@ -197,13 +240,13 @@ const player2 = Player("O",false,[])
 const machine = Player("O",false,[])
 let gamesIsOver = false
 let signs = 0
+let bestCore = -Infinity
 
 const gameBoard = (() => {
     const board = ["","","",
                    "","","",
                    "","",""]
-    
-    
+
     const setBox = (index,sign) => {
         if (index > board.length) return;
         board[index] = sign
@@ -244,6 +287,10 @@ const checkWinner = (() => {
            } 
         }
      }
-     return{ checkPlayerWinner,gamesIsOver }
+     return{ checkPlayerWinner }
 })()
-    
+ const scores =  {
+    X: 1,
+    O: -1,
+    tie:0
+ }
